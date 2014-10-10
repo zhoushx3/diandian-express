b5a99d8ca5e318e4c3955b06c74c26ad5b5c86d2
ZHOU <1006487900@qq.com>
2014-10-10 19:11:14 +0800

    后台相册及子页面数据库操作完成

--- public/javascripts/background_albums_details.js
+++ public/javascripts/background_albums_details.js
@@ -1,27 +1,17 @@
 (function() {
 // background_albums_details
 	if(location.pathname.indexOf('/background/albums/') > -1) {
+		$('.save').click(function() {
+			$('.hiddenPictureNames').attr('value', "");
+			$('.hiddenAlbumCoverSrc').attr('value', $('#album_cover img').attr('src'));
+			for (var i = 0; i < $('.ui-state-default').children('textarea').length; ++i) {
+				$('.hiddenPictureIntroductions').attr('value', $('.hiddenPictureIntroductions').attr('value') + "***" + $('.ui-state-default').children('textarea').eq(i).val());
+				$('.hiddenPictureNames').attr('value', $('.hiddenPictureNames').attr('value') + "***" + $('.ui-state-default').children('textarea').eq(i).attr('name'));
+			}
+		});
 		// jquery UI --- sortable 
-				$( "#album_pitures_list" ).sortable();
-		// $(.add) click event is to add a new img , here without copy event
-				$('.add').click(function() { 
-					$('#album_pitures_list').prepend(	
-						'<div class="ui-state-default  ui-sortable-handle">' +
-							'<img src="../../images/ini.png"/>' + 
-						 	' <textarea> </textarea>' +
-						  	'<div class="img_property">' +
-						   		'<input type="button" value="删除照片？" class="delete"/>' +
-						    	'<input type="button" value="设为封面？" class="setCover"/>'  +
-						 	'</div>'  +
-						  	'<div class="new_img_load">' + 
-						  		'<input type="file"/>'  +
- 						    	'<input type="button" value="删除照片？" class="delete"/>'  +
-						  	'</div>'  +
-						'</div>'
-					);
-				});
-
-// ! .live()	.delegate() ...http://api.jquery.com/live/ in order to bind events to any matched elements even prepend or  append 
+				// $( "#album_pitures_list" ).sortable();
+		// ! .live()	.delegate() ...http://api.jquery.com/live/ to bind events to any matched elements even prepend or  append 
 		// $(img).hover used to show 2 button.
 				$(document).delegate(".ui-state-default img","mouseover",  function(event) {
 					$(event.target).parent().children('.img_property').addClass('active');
@@ -34,24 +24,28 @@
 				$(document).delegate(".setCover", "click", function(event) {
 					$('#album_cover > img').attr('src', $(event.target).parent().parent().children('img').attr('src'));
 				});
+		// $(.add) click event is to add a new img , here without copy event
+				$('.add').click(function() {
+					$('#add_new_photos').addClass('active').children('form').css("animation", "myfirst 2s");
+				});
 	}
 
 // background_albums
 	if (location.pathname == '/background/albums') {
-				$( "#albums_list_show" ).sortable();
-
-		// $(.add) click event is to add a new img 
-				$('.add').click(function() {
-					$('#albums_list_show').prepend(
-						'<div class="ui-state-default ui-sortable-handle">' +
-							'<a href="#"><img src="/images/album_cover_ini.png" /></a>' +
-						 	'<textarea> </textarea>' +
-						 	'<div>' +
-							    '<input type="button" value="@" class="delete"/>' + 
-						 	'</div>' +
-						'</div>'
-					);
-				});
+				// $( "#albums_list_show" ).sortable();
+		// $(.add) click event is to add new imgs 
+			$('.add').click(function() {
+				$('#add_album').addClass('active').children('form').css("animation", "myfirst 2s");
+			});
+		// $('.save_all') click event is to save album_names
+			$('.save_all').click(function() {
+				$('.hiddenAlbumIds').attr('value', "");
+				$('.hiddenAlbumNames').attr('value', "");
+				for (var i = 0; i < $('.ui-state-default').children('textarea').length; ++i) {
+					$('.hiddenAlbumIds').attr('value', $('.hiddenAlbumIds').attr('value') + "***" + $('.ui-state-default').children('textarea').eq(i).attr('title'));
+					$('.hiddenAlbumNames').attr('value', $('.hiddenAlbumNames').attr('value') + "***" + $('.ui-state-default').children('textarea').eq(i).val());
+				}
+			});
 	}
 
 // background_albums and background_albums_details comon events
@@ -61,6 +55,8 @@
 					$('#make_sure').addClass('active');
 
 					$(document).delegate('.yes', 'click', function() {
+						$('.hiddenAlbumName').attr('value', $(event.target).parent().parent().children('textarea').attr('title'));
+						$('.hiddenAlbumId').attr('value', $(event.target).parent().parent().children('textarea').attr('name'));
 						$('#make_sure').removeClass('active');
 						$(event.target).parent().parent().remove();
 						// ! event.target returns a DOM then $() get a jquery object. 
@@ -71,6 +67,10 @@
 					$(document).delegate('.no', 'click', function() {
 						$('#make_sure').removeClass('active');
 					});
+				});
+		// $(.cancel) click event  cancel to add an new album or add new photos
+ 				$('.cancel').click(function() {
+ 					$(this).parent().parent().removeClass('active').children('form').css("animation", "");
 				});
 	}
 })();
--- test_data/albums.js
+++ test_data/albums.js
@@ -9,34 +9,11 @@ exports.insert = function(db, callback){
         throw new Error('获取users失败！');
       }
       var pictureIds = [];
-
-      //db.collection('AlbumsPictures', function(err, AlbumsPictures){
-        //if (err){
-          //throw new Error('获取Albumspictures失败！');
-        //}
-        //for (var i = 1; i <= 10; i++) {
-          //pictureIds.push(AlbumsPictures.insert({
-            //src: '/images/post1_' + i + '.jpg',
-            //name: '图片' + i,
-            //votes: [],
-            //commentCount: 0
-          //}, function(err, result){
-            //if (err){
-              //throw new Error('向AlbumsPictures插入数据失败！');
-            //}
-            //if (callback){
-              //callback();
-            //}
-          //}));
-        //}
-      //});
       
       for (var i = 1; i <= 10; i++) {
         pictureIds.push({
           src: '/images/post1_' + i + '.jpg',
           name: '\u56fe\u7247' + i,
-          votes: [],
-          photo_date: "2014-" + i + "-" + (i +10),
           instruction: "有什么好介绍的吗？没有有什么好介绍的吗？没有有什么好介绍的吗？没有有什么好介绍的吗？没有有什么好介绍的吗？没有"
         });
       }
@@ -50,7 +27,6 @@ exports.insert = function(db, callback){
           {
             name: 'album_1',
             createdAt: new Date(),
-            createdBy: doc._id,
             cover: '/images/post1_1.jpg',
             pictures: pictureIds
           },
@@ -58,7 +34,6 @@ exports.insert = function(db, callback){
           {
             name: 'album_2',
             createdAt: new Date(),
-            createdBy: doc._id,
             cover: '/images/post1_2.jpg',
             pictures: pictureIds.slice(0, 5)
           },
@@ -66,7 +41,6 @@ exports.insert = function(db, callback){
           {
             name: 'album_3',
             createdAt: new Date(),
-            createdBy: doc._id,
             cover: '/images/post1_6.jpg',
             pictures: pictureIds.slice(5)
           }
